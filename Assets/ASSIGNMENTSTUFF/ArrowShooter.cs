d7dd1a4ae24dd62c8b0a80b27f6115af81364b18
delmastv <delmastv@sheridancollege.ca>
2025-04-09 03:20:30 -0400

    made my second script...referenced the 1st script, started an if statement for key controls

---
+++ Assets/ASSIGNMENTSTUFF/ArrowShooter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShooter : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public WeaponManager weaponmanagerscript;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            
+        }
+    }
+}
